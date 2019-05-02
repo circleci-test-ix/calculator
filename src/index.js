8983649e854d8614d5e1cb8c6351266a41f45900
Colin Hemmings <colin.hemmings@gmail.com>
2019-05-02 16:11:32 +0200

    linting error

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import App from "./component/App";
 import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
+var bob = "SDfd"
 ReactDOM.render(<App />, document.getElementById("root"));
